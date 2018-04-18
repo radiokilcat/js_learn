b921d3a931d022d70a0158f4e0065a4e90210a7a
Alexander Plaksin <nojunejoe@gmail.com>
2018-04-19 02:23:03 +0300

    new publish

--- dist/bin/brain-games.js
+++ dist/bin/brain-games.js
@@ -1,11 +1,8 @@
 #!/usr/local/bin/node
+'use strict';
 
-const _ = require('..');
+var _ = require('..');
 
 (0, _.greeting)();
 (0, _.userGreeting)();
-/*  # sourceMappingURL=data:application/json;
-charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9iaW4vYnJhaW4
-tZ2FtZXMuanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFDQTs7QUFFQTtBQUNBIiwiZmlsZSI
-6ImJyYWluLWdhbWVzLmpzIiwic291cmNlc0NvbnRlbnQiOlsiXG5pbXBvcnQge2dyZWV0aW5nLCB1c2V
-yR3JlZXRpbmd9IGZyb20gJy4uJztcblxuZ3JlZXRpbmcoKTtcbnVzZXJHcmVldGluZygpO1xuIl19 */
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9iaW4vYnJhaW4tZ2FtZXMuanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFDQTs7QUFFQTtBQUNBIiwiZmlsZSI6ImJyYWluLWdhbWVzLmpzIiwic291cmNlc0NvbnRlbnQiOlsiXG5pbXBvcnQgeyBncmVldGluZywgdXNlckdyZWV0aW5nIH0gZnJvbSAnLi4nO1xuXG5ncmVldGluZygpO1xudXNlckdyZWV0aW5nKCk7XG4iXX0=
--- dist/index.js
+++ dist/index.js
@@ -1,35 +1,22 @@
+'use strict';
 
-
-Object.defineProperty(exports, '__esModule', {
-  value: true,
+Object.defineProperty(exports, "__esModule", {
+  value: true
 });
-exports.userGreeting = undefined;
-exports.greeting = undefined;
+exports.userGreeting = exports.greeting = undefined;
 
-const _readlineSync = require('readline-sync');
+var _readlineSync = require('readline-sync');
 
-const _readlineSync2 = _interopRequireDefault(_readlineSync);
+var _readlineSync2 = _interopRequireDefault(_readlineSync);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-const greeting = exports.greeting = function greeting() {
+var greeting = exports.greeting = function greeting() {
   return console.log('Welcome to the Brain Games!');
 };
 
-const userGreeting = exports.userGreeting = function userGreeting() {
-  const userName = _readlineSync2.default.question('May I have your name? ');
-  console.log(`hi ${userName} !`);
+var userGreeting = exports.userGreeting = function userGreeting() {
+  var userName = _readlineSync2.default.question('May I have your name? ');
+  console.log('hi ' + userName + ' !');
 };
-/* # sourceMappingURL=data:application/json;
-charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uL3NyYy9pbmRleC5qcyJdLCJ
-uYW1lcyI6WyJncmVldGluZyIsImNvbnNvbGUiLCJsb2ciLCJ1c2VyR3JlZXRpbmciLCJ1c2VyTmFtZSI
-sInF1ZXN0aW9uIl0sIm1hcHBpbmdzIjoiOzs7Ozs7O0FBQUE7Ozs7OztBQUdPLElBQU1BLDhCQUFXLFN
-BQVhBLFFBQVc7QUFBQSxTQUFNQyxRQUFRQyxHQUFSLENBQVksNkJBQVosQ0FBTjtBQUFBLENBQWpCOzt
-BQUVBLElBQU9DLHNDQUFlLFNBQWZBLFlBQWUsR0FBTTtBQUNqQyxNQUFNQyxXQUFXLHVCQUFhQyxRQUF
-iLENBQXNCLHdCQUF0QixDQUFqQjtBQUNBSixVQUFRQyxHQUFSLFNBQWtCRSxRQUFsQjtBQUNELENBSE0
-iLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgcmVhZGxpbmVTeW5jIGZ
-yb20gJ3JlYWRsaW5lLXN5bmMnO1xuXG5cbmV4cG9ydCBjb25zdCBncmVldGluZyA9ICgpID0+IGNvbnN
-vbGUubG9nKCdXZWxjb21lIHRvIHRoZSBCcmFpbiBHYW1lcyEnKTtcblxuZXhwb3J0IGNvbnN0ICB1c2V
-yR3JlZXRpbmcgPSAoKSA9PiB7XG4gIGNvbnN0IHVzZXJOYW1lID0gcmVhZGxpbmVTeW5jLnF1ZXN0aW9
-uKCdNYXkgSSBoYXZlIHlvdXIgbmFtZT8gJyk7XG4gIGNvbnNvbGUubG9nKGBoaSAke3VzZXJOYW1lfSA
-hYCk7XG59O1xuIl19 */
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uL3NyYy9pbmRleC5qcyJdLCJuYW1lcyI6WyJncmVldGluZyIsImNvbnNvbGUiLCJsb2ciLCJ1c2VyR3JlZXRpbmciLCJ1c2VyTmFtZSIsInF1ZXN0aW9uIl0sIm1hcHBpbmdzIjoiOzs7Ozs7O0FBQUE7Ozs7OztBQUdPLElBQU1BLDhCQUFXLFNBQVhBLFFBQVc7QUFBQSxTQUFNQyxRQUFRQyxHQUFSLENBQVksNkJBQVosQ0FBTjtBQUFBLENBQWpCOztBQUVBLElBQU1DLHNDQUFlLFNBQWZBLFlBQWUsR0FBTTtBQUNoQyxNQUFNQyxXQUFXLHVCQUFhQyxRQUFiLENBQXNCLHdCQUF0QixDQUFqQjtBQUNBSixVQUFRQyxHQUFSLFNBQWtCRSxRQUFsQjtBQUNELENBSE0iLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgcmVhZGxpbmVTeW5jIGZyb20gJ3JlYWRsaW5lLXN5bmMnO1xuXG5cbmV4cG9ydCBjb25zdCBncmVldGluZyA9ICgpID0+IGNvbnNvbGUubG9nKCdXZWxjb21lIHRvIHRoZSBCcmFpbiBHYW1lcyEnKTtcblxuZXhwb3J0IGNvbnN0IHVzZXJHcmVldGluZyA9ICgpID0+IHtcbiAgY29uc3QgdXNlck5hbWUgPSByZWFkbGluZVN5bmMucXVlc3Rpb24oJ01heSBJIGhhdmUgeW91ciBuYW1lPyAnKTtcbiAgY29uc29sZS5sb2coYGhpICR7dXNlck5hbWV9ICFgKTtcbn07XG4iXX0=
