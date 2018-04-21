43f69d634aa6f56cf96c33b699f7cc1f03251cf1
Alexander Plaksin <nojunejoe@gmail.com>
2018-04-21 16:00:14 +0300

    add brain-gcd.js

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -1,2 +1,22 @@
 import { cons } from 'hexlet-pairs';
 import { getRandomInt } from '..';
+
+const findGcd = (a, b) => {
+  if (a > b) {
+    if (a % b === 0) {
+      return b;
+    }
+    return findGcd(b, a % b);
+  }
+  if (a % b === 0) {
+    return b;
+  }
+  return findGcd(a, b % a);
+};
+
+const quest = () => {
+  const first = getRandomInt(1, 100);
+  const second = getRandomInt(1, 100);
+  return cons(`${first}, ${second}`, String(findGcd(first, second)));
+};
+export default quest;
