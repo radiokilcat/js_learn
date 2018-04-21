54122837dfb8b45bc4ce396c0dcef41463e5c113
Alexander Plaksin <nojunejoe@gmail.com>
2018-04-21 16:27:53 +0300

    brain-gcd fixed, package updated

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -2,18 +2,11 @@ import { cons } from 'hexlet-pairs';
 import { getRandomInt } from '..';
 
 const findGcd = (a, b) => {
-  if (a > b) {
-    if (a % b === 0) {
-      return b;
-    }
-    return findGcd(b, a % b);
+  if (!b) {
+    return a;
   }
-  if (a % b === 0) {
-    return b;
-  }
-  return findGcd(a, b % a);
+  return findGcd(b, a % b);
 };
-
 const quest = () => {
   const first = getRandomInt(1, 100);
   const second = getRandomInt(1, 100);
