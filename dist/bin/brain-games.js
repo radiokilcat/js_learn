d4fa676ec3b3f48c5d690c20bf20919d8694f646
Alexander Plaksin <nojunejoe@gmail.com>
2018-04-21 13:50:53 +0300

    remove dist from repo

--- dist/bin/brain-games.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-'use strict';
-
-var _ = require('..');
-
-(0, _.greeting)();
-(0, _.userGreeting)();
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9iaW4vYnJhaW4tZ2FtZXMuanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFDQTs7QUFFQTtBQUNBIiwiZmlsZSI6ImJyYWluLWdhbWVzLmpzIiwic291cmNlc0NvbnRlbnQiOlsiXG5pbXBvcnQgeyBncmVldGluZywgdXNlckdyZWV0aW5nIH0gZnJvbSAnLi4nO1xuXG5ncmVldGluZygpO1xudXNlckdyZWV0aW5nKCk7XG4iXX0=
